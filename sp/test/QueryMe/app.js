a4bc22a3248476016ea756dd54d068f71104d130
Gal Sternad <gal.sternad@student.um.si>
2023-04-22 20:52:29 +0200

    SLOAIR-77

--- sp/test/QueryMe/app.js
+++ sp/test/QueryMe/app.js
@@ -19,7 +19,7 @@ const answerRouter = require('./routes/answerRoutes');
 // DB connection
 const mongoose = require('mongoose');
 const mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB, { useNewURLParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
